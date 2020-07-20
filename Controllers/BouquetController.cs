f70c0401922249527ea530fb8c6cd386647b3a89
evaartray@outlook.com <evaartray@outlook.com>
2020-07-20 23:34:10 +0200

    Fixed carousel on the index page, added new model with a controller and the view (edit and details)

---
+++ Controllers/BouquetController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineShop.Models;
+
+namespace OnlineShop.Controllers
+{
+    public class BouquetController : Controller
+    {
+        private static List<Bouquet> bouquets = new List<Bouquet>
+        {
+            new Bouquet(1, "Spring lilac and roses bouquet", 299),
+            new Bouquet(2, "Summer Joy bouquet", 219),
+            new Bouquet(3, "Purple dream bouquet", 301)
+            
+        };
+
+        // GET: Bouquet
+        public ActionResult Index()
+        {
+            return View(bouquets);
+        }
+
+        // GET: Bouquet/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Bouquet/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bouquet/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Bouquet/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var bouquet = bouquets.Single(obj => obj.BouquetId == id);
+            return View(bouquet);
+        }
+
+        // POST: Bouquet/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                bouquets.Single(obj => obj.BouquetId == id).Name = collection["Name"];
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Bouquet/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Bouquet/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Bouquet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Models
+{
+    public class Bouquet
+    {
+        public int BouquetId { get; set; }
+        public string Name { get; set; }
+        public int Cost { get; set; }
+
+        public Bouquet(int bouquetId, string name, int cost)
+        {
+            BouquetId = bouquetId;
+            Name = name;
+            Cost = cost;
+
+        }
+    }
+}
